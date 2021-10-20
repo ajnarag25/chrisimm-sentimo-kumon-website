025848641474e258a0c401d1a2258e3194e9cef9
Eyyjayy <78248093+ajnarag25@users.noreply.github.com>
2021-10-20 18:26:38 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slider");
+        }
+    });
+  });
+  
+})
